7ce36ddbf505ee8a959273c7833552235c2740b1
John Mark Gabriel Caguicla <jmg@caguicla.me>
2018-05-30 20:41:29 +0800

    Add more tests from WanaKana

--- Test/HepburnConverterTest.cs
+++ Test/HepburnConverterTest.cs
@@ -35,7 +35,7 @@ namespace WanaKanaSharp.Test
 	[TestFixture()]
 	public class HepburnConverterTest
 	{
-		[TestCase(null, ExpectedResult = null)]
+		[TestCase(null, ExpectedResult = "")]
 		[TestCase("", ExpectedResult = "")]
 		[TestCase("ワニカニ　ガ　スゴイ　ダ", ExpectedResult = "wanikani ga sugoi da")]
 		[TestCase("わにかに　が　すごい　だ", ExpectedResult = "wanikani ga sugoi da")]
@@ -46,6 +46,25 @@ namespace WanaKanaSharp.Test
 		[TestCase("ばつげーむ", ExpectedResult = "batsuge-mu")]
 		[TestCase("一抹げーむ", ExpectedResult = "一抹ge-mu")]
 		[TestCase("スーパー", ExpectedResult = "suupaa")]
+		[TestCase("缶コーヒー", ExpectedResult = "缶koohii")]
+		// TODO: Add missing test case
+		[TestCase("きんにくまん", ExpectedResult = "kinnikuman")]
+		[TestCase("んんにんにんにゃんやん", ExpectedResult = "nnninninnyan'yan")]
+		[TestCase("かっぱ　たった　しゅっしゅ ちゃっちゃ　やっつ", ExpectedResult = "kappa tatta shusshu chatcha yattsu")]
+		[TestCase("っ", ExpectedResult = "")]
+		[TestCase("ヶ", ExpectedResult = "ヶ")]
+		[TestCase("ヵ", ExpectedResult = "ヵ")]
+		[TestCase("ゃ", ExpectedResult = "ya")]
+		[TestCase("ゅ", ExpectedResult = "yu")]
+		[TestCase("ょ", ExpectedResult = "yo")]
+		[TestCase("ぁ", ExpectedResult = "a")]
+		[TestCase("ぃ", ExpectedResult = "i")]
+		[TestCase("ぅ", ExpectedResult = "u")]
+		[TestCase("ぇ", ExpectedResult = "e")]
+		[TestCase("ぉ", ExpectedResult = "o")]
+		[TestCase("おんよみ", ExpectedResult = "on'yomi")]
+		[TestCase("んよ んあ んゆ", ExpectedResult = "n'yo n'a n'yu")]
+		[TestCase("シンヨ", ExpectedResult = "shin'yo")]
 		public String Convert(String input, Boolean upcaseKatakana = false, Trie<Char, String> customRomajiMapping = null) => HepburnConverter.Convert(input, upcaseKatakana, customRomajiMapping);
 	}
 }
